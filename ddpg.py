cc1023c0c2e1e915be6b85e22efe228cad67547c
thiago <stargaze221@gmail.com>
2017-12-02 15:29:16 -0600

    2nd order plant

--- ddpg.py
+++ ddpg.py
@@ -20,12 +20,12 @@ import matplotlib.pyplot as plt
 
 OU = OU()       #Ornstein-Uhlenbeck Process
 
-def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
-    BUFFER_SIZE = 100000
+def playGame(train_indicator=1):    #1 means Train, 0 means simply Run
+    BUFFER_SIZE = 1000000
     BATCH_SIZE = 32
     GAMMA = 0.99
     TAU = 0.002     #Target Network HyperParameters
-    LRA = 0.00025    #Learning rate for Actor
+    LRA = 0.0002    #Learning rate for Actor
     LRC = 0.001     #Lerning rate for Critic
 
     action_dim = 1  #Steering/Acceleration/Brake
@@ -37,8 +37,8 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
 
     EXPLORE = 100000.
     episode_count = 4000
-    max_steps = 200
-    reward = -1000
+    max_steps = 10000
+    reward = 0
     done = False
     step = 0
     epsilon = 1
@@ -94,7 +94,7 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
             #s_t2 =np.expand_dims(s_t,axis = 1)
             a_t_original = actor.model.predict(s_t)
 
-            noise_t = train_indicator * max(epsilon, 0) * OU.function(a_t_original, 0 , 0.0, 5)
+            noise_t = train_indicator * max(epsilon, 0) * (OU.function(a_t_original, 0 , 0.0,7 ) + 0.1*env.disturbance(env.time))
             #noise_t[0][1] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][1],  0.5 , 1.00, 0.10)
             #noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2], -0.1 , 1.00, 0.05)
 
@@ -104,6 +104,7 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
             #    noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2],  0.2 , 1.00, 0.10)
 
             a_t = a_t_original + noise_t
+            a_t = a_t/a_t
             #a_t[0][1] = a_t_original[0][1] + noise_t[0][1]
             #a_t[0][2] = a_t_original[0][2] + noise_t[0][2]
 
@@ -145,12 +146,13 @@ def playGame(train_indicator=0):    #1 means Train, 0 means simply Run
             step += 1
             if done:
                 break
-        plt.close()
-        plt.plot(env.hist)
-        plt.plot(env.mag*np.sin(env.freq*np.linspace(0,env.time,max_steps))+env.bias, 'r')
-        #plt.ylim([-1, 2])
-        plt.show(block=False)
-        #plt.draw()
+        if np.mod(i, 10) == 0:
+            plt.close()
+            plt.plot(env.hist)
+            plt.plot(env.ref_hist, 'r')
+            #plt.ylim([-1, 2])
+            plt.show(block=False)
+            #plt.draw()
         if np.mod(i, 3) == 0:
 
             if (train_indicator):
--- model/lti.py
+++ model/lti.py
@@ -3,12 +3,13 @@ import scipy.integrate as integrate
 from math import sin, cos
 class LTI:
     def __init__(self, state_0):
-        """ pos = [x,y,z] attitude = [rool,pitch,yaw]
-            """
-        self.state = state_0
-        self.desired_state = 0.0
-        self.s_dot = 0
+        self.state = np.zeros(2)
+        self.state[0] = state_0
+        self.state[1] = 0
+        self.ref = 0.0
+        self.x_dot = np.zeros(2)
         self.hist = []
+        self.ref_hist = []
         self.time = 0.0
         control_frequency = 100 # Hz for attitude control loop
         self.dt = 1.0 / control_frequency
@@ -16,51 +17,56 @@ class LTI:
         self.freq = 1
         self.bias = 0
         self.noise_rand = np.zeros(5)
-
+        self.a1 = 1
+        self.a2 = 1
 
     def reset(self):
-        self.state = np.random.rand()*2-0.5
+        self.state[0] = np.random.rand()*0
+        self.state[1] = np.random.randn()*0
         self.hist = []
+        self.ref_hist = []
         self.time = 0.0
-        self.mag = np.random.randn()*1.2
-        self.freq = 3*np.random.randn() + 2
-        self.bias = np.random.rand()
+        self.mag = np.random.randn()*0 +1
+        self.freq = 3*np.random.randn()*0
+        self.bias = np.random.rand()*0+1
         self.noise_rand = np.random.rand(5)
 
-        return self.state
+        return self.state[0] - self.ref
+    def disturbance(self,time):
+        w = self.noise_rand
+        return 0.1**w[0]*sin(time*10*w[1]) + 0.3*w[2]*cos(time*w[3]*9) - 0.5*sin(w[4]*time*6 + 3)
 
     def state_dot(self, state, t, u, time):
-        x1 = state
-        b = 1
-        a = 3
-        self.s_dot = 0.0
-        w = self.noise_rand
-        d =0.1**w[0]*sin(time*10*w[1]) + 0.3*w[2]*cos(time*w[3]*9) - 0.5*sin(w[4]*time*6 + 3)
-        self.s_dot  = -a*x1 + b*u + d
+        x1 = state[0]
+        x2 = state[1]
+        x1_dot = x2
+        x2_dot = -self.a1*x1-self.a2*x2 + u# + self.disturbance(time)
 
-        return self.s_dot
+        self.x_dot  = np.array([x1_dot, x2_dot])
 
-    def reward(self,s,u):
-        return -12*(s)**2 - 0.2*u**2
+        return self.x_dot
+
+    def reward(self,e,u):
+        if e < 1.2*np.exp(-0.2*self.time)+0.1:
+            return 1
+        return 0
+        #return -0*(e)**2 - 0*u**2 + 0.1
 
     def update(self, u):
         #saturate u
         #u = np.clip(u,-5,5)
         out = integrate.odeint(self.state_dot, self.state, [0,self.dt], args = (u,self.time))
         self.time += self.dt
-        #print out
         self.state = out[1]
         self.hist.append(np.array(self.state[0]))
 
     def step(self, action):
-        desired_state = 0
+        self.ref = self.mag*np.sin(self.freq*self.time) + self.bias
         done = False
-        #action = agent.do_action(env.state)
         self.update(action)
-        state = self.state[0] - self.mag*np.sin(self.freq*self.time) -self.bias
-        reward = self.reward(state, action)
-        if abs(self.state) > 3 or abs(self.s_dot)>50:
+        error = self.state[0] - self.ref
+        reward = self.reward(error, action)
+        self.ref_hist.append(np.array(self.ref))
+        if abs(error**2) > 2*np.exp(-0.08*self.time)+0.3 or abs(self.x_dot[1])>50:
             done = True
-
-        #return self.state[0], reward, done, {}
-        return state, reward, done, {}
+        return error, reward, done, {}
